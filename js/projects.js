b97bca0959e929d7010d6c586fa5d751a0235686
nestor <nestorgomez123450@gmail.com>
2026-06-30 12:41:37 +0200

    links y mejoras sobre las cards

--- js/projects.js
+++ js/projects.js
@@ -5,7 +5,7 @@ const projects = [
         tags: ["Python", "Tailwind", "JavaScript"],
         longTags: ["HTML", "Tailwind", "JavaScript", "Capacitor", "Sql", "Firebase", "Node"],
         longDescription: "Crewcall es una aplicación móvil creada para facilitar la comunicación eficiente entre los propietarios (owners) de grandes yates y su tripulación. A través de la aplicación, los owners pueden enviar mensajes a la tripulación, solicitar servicios y gestionar distintas tareas. Está desarrollada principalmente con tecnologías web para el frontend y Python para el backend, y posteriormente se empaqueta mediante la librería Capacitor para su despliegue en dispositivos iOS y Android. La aplicación cuenta con un sistema de roles, notificaciones push, creación y envío de tareas (no en tiempo real), gestión de fotos de perfil, recuperación de cuenta, entre otras funcionalidades.",
-        video: "https://www.youtube.com/embed/8O6ue7A2LnI?si=4YpDER60fknS5CXo",
+        video: "https://www.youtube.com/embed/CIEkzt9Oj1s?si=TdGuPiSJWq3Pl3Kb",
         github: "...",
     },
     {
@@ -14,7 +14,7 @@ const projects = [
         tags: ["React", "Expo"],
         longTags: ["React", "Expo", "Tailwind"],
         longDescription: "Se trata de una aplicación sencilla creada para probar la inteligencia artificial de detección de objetos en tiempo real mediante YOLOv8nano. La desarrollé con el objetivo de experimentar con nuevas tecnologías para mí, como React Native y Expo, además de explorar su integración en una aplicación móvil.",
-        video: "https://www.youtube.com/embed/UQV0htvvC4s?si=qj1t44SRV_xzoxbT",
+        video: "https://www.youtube.com/embed/KmcpZ8q42ac?si=bBiPWsxW6E1yEkjL",
         github: "...",
 
     },
@@ -24,7 +24,7 @@ const projects = [
         tags: ["PHP", "Sql", "JavaScript"],
         longTags: ["HTML", "Tailwind", "JavaScript", "Sql"],
         longDescription: "Desarrollé una aplicación para una empresa que digitaliza todo el proceso de firma de partes de trabajo (constancias de trabajo), sustituyendo el flujo tradicional en papel. La aplicación permite tanto a administradores como a trabajadores firmar documentos de forma digital, así como consultar el histórico de partes de manera organizada. Está diseñada para dos departamentos distintos, adaptando pequeños aspectos de la interfaz según el rol del usuario que inicia sesión. La aplicación está desarrollada con HTML, Tailwind CSS, JavaScript y PHP.",
-        video: "https://www.youtube.com/embed/UQV0htvvC4s?si=qj1t44SRV_xzoxbT",
+        video: "https://www.youtube.com/embed/5MwztbAbWc8?si=DCxvMHzMqgcttXeY",
         github: "...",
 
     },
@@ -34,7 +34,7 @@ const projects = [
         tags: ["React", "Rest API", "Supabase"],
         longTags: ["React", "Rest API", "Gemini", "Tailwind", "Supabase"],
         longDescription: "He desarrollado una aplicación que utiliza la inteligencia artificial de Gemini para ofrecer asistencia en la detección y corrección de errores en texto o imágenes. La aplicación integra el servicio de Supabase para la autenticación mediante Google y la gestión de la base de datos. Su funcionamiento es sencillo: el usuario envía una imagen o texto a través de la API de Google Gemini, y la IA devuelve el contenido con los errores marcados junto con una explicación de por qué se producen. El front-end está construido con React y un poco de Tailwind CSS.",
-        video: "https://www.youtube.com/embed/UQV0htvvC4s?si=qj1t44SRV_xzoxbT",
+        video: "https://www.youtube.com/embed/HYpjrLcPPfg?si=YKMFrFYftkCv6q_G",
         github: "...",
 
     }
@@ -51,7 +51,8 @@ function renderProjects() {
             <div class="card absolute inset-0 rounded-3xl
                         bg-white backdrop-blur-xl
                         border border-white/20
-                        shadow-xl p-5 flex flex-col justify-between">
+                        shadow-xl p-5 flex flex-col justify-between
+                        touch-none select-none [will-change:transform]">
 
                 <div>
                     <h3 class="text-lg font-semibold text-black">
@@ -139,5 +140,19 @@ document.getElementById("closeModal").addEventListener("click", () => {
 
     iframe.src = "";
 
+    document.body.style.overflow = "";
+});
+
+document.getElementById("openContactModal").addEventListener("click", () => {
+    const modal = document.getElementById("ContactModal");
+    modal.classList.remove("hidden");
+    modal.classList.add("flex");
+    document.body.style.overflow = "hidden";
+});
+
+document.getElementById("closeContactModal").addEventListener("click", () => {
+    const modal = document.getElementById("ContactModal");
+    modal.classList.add("hidden");
+    modal.classList.remove("flex");
     document.body.style.overflow = "";
 });
